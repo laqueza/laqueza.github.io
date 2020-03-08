a36f95f51aa52a4a10032eff66a78879338c9177
Mc Auriel Gagan <magagan@olx.ph>
2020-03-08 13:48:33 +0800

    update website theme 1

---
+++ lib/laqueza.rb
@@ -0,0 +1,6 @@
+require "laqueza/version"
+
+module Laqueza
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/laqueza_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Laqueza do
+  it "has a version number" do
+    expect(Laqueza::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
